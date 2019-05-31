704f9047193f744862a8ff04be0d345e61bbb29a
Carr <wallace2.carr@level3.com>
2019-05-31 12:10:02 -0600

    first commit

---
+++ ActivationObject.py
@@ -0,0 +1,72 @@
+class ActivationObject:
+    def __init__(self):
+        print("Woo!")
+
+    def setActivationDate(self, activationDate):
+        self.activationDate = activationDate
+    def getActivationDate(self)
+        return self.activationDate
+
+    def setDay(self, day):
+        self.day = day
+    def getDay(self):
+        return self.day
+
+    def setDateTime(self,dateTime):
+        self.dateTime = dateTime
+    def getDateTime(self):
+        return self.dateTime
+
+    def setLocalTimeActivation(self,localTimeActivation):
+        self.localTimeActivation = localTimeActivation
+    def getLocalTimeActivation(self):
+        return self.localTimeActivation
+
+    def setMtnTimeActivation(self,mtnTimeActivation):
+        self.mtnTimeActivation = mtnTimeActivation
+    def getMtnTimeActivation(self):
+        return self.mtnTimeActivation
+
+    def setServiceActivationTech(self, serviceActivationTech):
+        self.serviceActivationTech = serviceActivationTech
+    def getServiceActivationTech(self):
+        return self.serviceActivationTech
+
+    def setStoreNumber(self,storeNumber):
+        self.storeNumber = storeNumber
+    def getStoreNumber(self):
+        return self.storeNumber
+
+    def setAddress(self, address):
+        self.address = address
+    def getAddress(self):
+        return self.address
+
+    def setBridgeInformation(self, bridgeInformation):
+        self.bridgeInformation = bridgeInformation
+    def getBridgeInformation(self, bridgeInformation):
+        return self.bridgeInformation
+
+    def setRapidDeployEon(self, rapidDeployEon):
+        self.rapidDeployEon = rapidDeployEon
+    def getRapidDeployEon(self):
+        return self.rapidDeployEon
+
+    def setTargetDeployEon(self, targetDeployEon):
+        self.targetDeployEon = targetDeployEon
+    def getTargetDeployEon(self):
+        return self.targetDeployEon
+
+    def setOpsConsoleTicketNumber(self, opsConsoleTicketNumber):
+        self.opsConsoleTicketNumber = opsConsoleTicketNumber
+    def getOpsConsoleTicketNumber(self):
+        return self.opsConsoleTicketNumber
+
+    def setServiceActivationTechOpsConsoleTicketNumber(self, serviceActivationTechOpsConsoleTicketNumber):
+        self.serviceActivationTechOpsConsoleTicketNumber = serviceActivationTechOpsConsoleTicketNumber
+    def getServiceActivationTechOpsConsoleTicketNumber(self):
+        return self.serviceActivationTechOpsConsoleTicketNumber
+
+    def setSupportVendor(self, supportVendor):
+        self.supportVendor = supportVendor
+    def
